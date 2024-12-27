76e936c5c60197358b2af4f79d28dfe554819886
sttadic <stjepan.tadich@gmail.com>
2024-12-27 22:05:38 +0000

    Add documentation for Menu class.

--- src/ie/atu/sw/Menu.java
+++ src/ie/atu/sw/Menu.java
@@ -2,6 +2,12 @@ package ie.atu.sw;
 
 import static java.lang.System.out;
 
+/**
+ * The class Menu serves as the main user interface, acts as the entry point and
+ * controls the flow of the application. It displays varoius menu options,
+ * selected paths and algorithms to the user and initiates processing based on
+ * user's configuration.
+ */
 public class Menu {
 	private InputHandler inputHandler;
 	private ConfigHandler configHandler;
@@ -9,13 +15,19 @@ public class Menu {
 	// Error message displayed within the options menu
 	private String errorMsg;
 
+	/**
+	 * Instantiates a Menu object and initializes InputHandler and ConfigHandler.
+	 */
 	public Menu() {
 		this.inputHandler = new InputHandler();
 		this.configHandler = new ConfigHandler(this.inputHandler);
 	}
 
-	
-	// Start the application
+	/**
+	 * Starts the application by running a loop that displays menu options, reads
+	 * user input and handles selected option until user quits or application
+	 * completes.
+	 */
 	public void startApplication() {
 		while (keepRunning) {
 			showOptions();
@@ -24,7 +36,13 @@ public class Menu {
 		inputHandler.closeScanner();
 		out.println("Thank you for using Simplifying Text with Word Embeddings!");
 	}
-	
+
+	/**
+	 * Processes menu selection and invokes corresponding method based on user
+	 * choice. If invalid choice is made, an error message is displayed.
+	 * 
+	 * @param choice the user's input associated with menu option
+	 */
 	private void handleChoice(int choice) {
 		switch (choice) {
 		case 1 -> configHandler.setEmbeddingsPath();
@@ -32,13 +50,16 @@ public class Menu {
 		case 3 -> configHandler.setOutputPath();
 		case 4 -> configHandler.setCommonWordsPath();
 		case 5 -> comparisonAlgorithmSelection();
-		case 6 -> keepRunning = false;   // Start of processing to be implemented
+		case 6 -> keepRunning = false; // Start of processing to be implemented
 		case 7 -> keepRunning = false;
 		default -> errorMsg = "Invalid Selection! Please use one of the options above";
 		}
 	}
 
-	// Define an algoritham used to calculate distance between vectors
+	/**
+	 * Prompts the user to select a vector comparison algorithm and delegates
+	 * selection to the ConfigHandler to set the chosen algorithm.
+	 */
 	private void comparisonAlgorithmSelection() {
 		clearScreen();
 		out.println(ConsoleColour.WHITE_BOLD);
@@ -50,11 +71,15 @@ public class Menu {
 		out.println("(3) Dot Product");
 		out.println("(4) Combine All");
 		out.println();
-		
+
 		configHandler.setComparisonAlgorithm();
 	}
 
-	// Menu options
+	/**
+	 * Displays the available options to the user, showing current configuration
+	 * settings. It also displays an error message if an error occurs during the
+	 * application execution."
+	 */
 	private void showOptions() {
 		clearScreen();
 		out.println(ConsoleColour.WHITE);
@@ -65,20 +90,25 @@ public class Menu {
 		out.println("*                                                          *");
 		out.println("************************************************************");
 		// Display embeddings file path
-		out.println("(1) Specify Embedding File  ---> " + ConsoleColour.GREEN + configHandler.getEmbeddingsFilePath() + ConsoleColour.WHITE);
+		out.println("(1) Specify Embedding File  ---> " + ConsoleColour.GREEN + configHandler.getEmbeddingsFilePath()
+				+ ConsoleColour.WHITE);
 		// Display input file path
-		out.println("(2) Text File to Simplify   ---> " + ConsoleColour.GREEN + configHandler.getInputFilePath() + ConsoleColour.WHITE);
+		out.println("(2) Text File to Simplify   ---> " + ConsoleColour.GREEN + configHandler.getInputFilePath()
+				+ ConsoleColour.WHITE);
 		// Display output file path
-		out.println("(3) Specify an Output file  ---> " + ConsoleColour.GREEN + configHandler.getOutputFilePath() + ConsoleColour.WHITE);
+		out.println("(3) Specify an Output file  ---> " + ConsoleColour.GREEN + configHandler.getOutputFilePath()
+				+ ConsoleColour.WHITE);
 		// Display common words file path
-		out.println("(4) Most Common Words File  ---> " + ConsoleColour.GREEN + configHandler.getCommonWordsFilePath() + ConsoleColour.WHITE);
+		out.println("(4) Most Common Words File  ---> " + ConsoleColour.GREEN + configHandler.getCommonWordsFilePath()
+				+ ConsoleColour.WHITE);
 		// Display algorithm used for comparison
-		out.println("(5) Set Comparison Method   ---> " + ConsoleColour.GREEN + configHandler.getVectorComparisonAlgo() + ConsoleColour.WHITE);
+		out.println("(5) Set Comparison Method   ---> " + ConsoleColour.GREEN + configHandler.getVectorComparisonAlgo()
+				+ ConsoleColour.WHITE);
 		// Start simplifying text
 		out.println("(6) START PROCESSING");
 		// Quit
 		out.println("(7) Quit");
-		
+
 		// Option selection block
 		out.println(ConsoleColour.WHITE_BOLD);
 		out.println("");
@@ -90,9 +120,10 @@ public class Menu {
 		}
 	}
 
-	/*
-	 * Source: https://intellipaat.com/community/294/java-clear-the-console 
-	 * Clears terminal window (doesn't work for IDE console)
+	/**
+	 * Clears the terminal screen. Does not work in all environments like IDE
+	 * consoles. Source:
+	 * https://intellipaat.com/community/294/java-clear-the-console
 	 */
 	public static void clearScreen() {
 		out.print("\033[H\033[2J");
