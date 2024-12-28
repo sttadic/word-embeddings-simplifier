7c65073bab0109f7536535d9d8419da4db904b63
sttadic <stjepan.tadich@gmail.com>
2024-12-28 22:59:00 +0000

    Implement tokanization and tokens are stored in a list. Tokens are represented as a Map.Entry<String, Boolean> where String is token content, and boolean indicates if the String is a word or not.

--- src/ie/atu/sw/InputTextParser.java
+++ src/ie/atu/sw/InputTextParser.java
@@ -3,19 +3,39 @@ package ie.atu.sw;
 import java.io.*;
 import java.util.*;
 
-public class InputTextParser implements FileParser<List<String>>{
+public class InputTextParser implements FileParser<List<Map.Entry<String, Boolean>>>{
 	
 	@Override
-	public List<String> parse(String filePath) throws IOException {
-		var inputTextList = new ArrayList<String>();
+	public List<Map.Entry<String, Boolean>> parse(String filePath) throws IOException {
+		var tokens = new ArrayList<Map.Entry<String, Boolean>>();
 		
 		try(var br = new BufferedReader(new InputStreamReader(new FileInputStream(filePath)))) {
 			String line;
 			while((line = br.readLine()) != null) {
-				String[] wordsArr = line.split("\\W+");
-				Collections.addAll(inputTextList, wordsArr);
+				tokens.addAll(tokenize(line));
 			}
 		}
-		return inputTextList;
+		return tokens;
+	}
+	
+	private List<Map.Entry<String, Boolean>> tokenize(String line) {
+		var lineTokens = new ArrayList<Map.Entry<String, Boolean>>();
+		var token = new StringBuilder();
+		
+		for (char c : line.toCharArray()) {
+			if (Character.isLetterOrDigit(c)) {
+				token.append(c);
+			} else {
+				if (token.length() > 0) {
+					lineTokens.add(Map.entry(token.toString(), true));
+					token.setLength(0);
+				}
+				lineTokens.add(Map.entry(String.valueOf(c), false));
+			}
+		}
+		if (token.length() > 0) {
+			lineTokens.add(Map.entry(token.toString(), true));
+		}
+		return lineTokens;
 	}
 }
