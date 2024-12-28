10839d87605102beaa2524fea54d96c14f9f3282
sttadic <stjepan.tadich@gmail.com>
2024-12-28 20:58:15 +0000

    Implement InputTextParser to parse text that will be simplified.

---
+++ src/ie/atu/sw/InputTextParser.java
@@ -0,0 +1,21 @@
+package ie.atu.sw;
+
+import java.io.*;
+import java.util.*;
+
+public class InputTextParser implements FileParser<List<String>>{
+	
+	@Override
+	public List<String> parse(String filePath) throws IOException {
+		var inputTextList = new ArrayList<String>();
+		
+		try(var br = new BufferedReader(new InputStreamReader(new FileInputStream(filePath)))) {
+			String line;
+			while((line = br.readLine()) != null) {
+				String[] wordsArr = line.split("\\W+");
+				Collections.addAll(inputTextList, wordsArr);
+			}
+		}
+		return inputTextList;
+	}y
+}
