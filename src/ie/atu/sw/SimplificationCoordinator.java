0d88ae8ac7d8fb9d8cdabb566b22f7c6775cc20d
sttadic <stjepan.tadich@gmail.com>
2024-12-30 18:02:26 +0000

    Change to more descriptive method name in SimplificationCoordinator. Implement basic logic for coordination.

--- src/ie/atu/sw/SimplificationCoordinator.java
+++ src/ie/atu/sw/SimplificationCoordinator.java
@@ -6,15 +6,27 @@ public class SimplificationCoordinator {
 	private final Map<String, double[]> embedMap;
 	private final Map<String, double[]> commonEmbedMap;
 	private final List<Map.Entry<String, Boolean>> toSimplifyList;
-	private final String comparisonAlg;
 
 	public SimplificationCoordinator(Map<String, double[]> embedMap, Map<String, double[]> commonEmbedMap,
-			List<Map.Entry<String, Boolean>> toSimplifyList, String comparisonAlg) {
+			List<Map.Entry<String, Boolean>> toSimplifyList) {
 		this.embedMap = embedMap;
 		this.commonEmbedMap = commonEmbedMap;
 		this.toSimplifyList = toSimplifyList;
-		this.comparisonAlg = comparisonAlg;
 	}
-	
-	
+
+	public void coordinateSimplification(String comparisonAlg) {
+		toSimplifyList.forEach(e -> {
+			if (e.getValue()) {
+				if (commonEmbedMap.containsKey(e.getKey())) {
+					// write e.getKey as it is since it is a common word
+				} else {
+					// run similarity analysis (pass in comparisonAlg)
+					// write most similar word from commonEmbedMap instead
+				}
+			} else {
+				// write e.getKey as it is since it's not a word (punctuation or space)
+			}
+		});
+	}
+
 }
