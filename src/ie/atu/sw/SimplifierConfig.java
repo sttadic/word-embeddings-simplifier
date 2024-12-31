a1fb9bc0469a9f2668e89f9a938e4c33f2064ca8
sttadic <stjepan.tadich@gmail.com>
2024-12-31 17:25:31 +0000

    Update java docs for SimplifierConfig recored. Change parameter name of vectorComparisonAlgo to vectorSimilarityAlg.

--- src/ie/atu/sw/SimplifierConfig.java
+++ src/ie/atu/sw/SimplifierConfig.java
@@ -2,7 +2,15 @@ package ie.atu.sw;
 
 /**
  * Record to encapsulate configuration settings for the application.
+ * 
+ * @param embeddingsFilePath  the file path to the word embeddings file
+ * @param inputFilePath       the file path to the text file that needs to be
+ *                            simplified
+ * @param outputFilePath      the file path where simplified text will be saved
+ * @param commonWordsFilePath the file path to most common words file
+ * @param vectorSimilarityAlg the name of the vector similarity algorithm to be
+ *                            used
  */
 public record SimplifierConfig(String embeddingsFilePath, String inputFilePath, String outputFilePath,
-		String commonWordsFilePath, String vectorComparisonAlgo) {
+		String commonWordsFilePath, String vectorSimilarityAlg) {
 }
